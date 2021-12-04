8a6c4dc6b814cde3e130c1854e1cf4dbd3afe559
Ian Laird <irlaird@gmail.com>
2021-12-03 21:23:53 -0700

    Added string parser

--- aocfw/parsers.py
+++ aocfw/parsers.py
@@ -6,3 +6,8 @@ from .typing import IParser
 class IntegerParser(IParser):
     def parse(self, data: IO) -> Iterable[int]:
         return map(int, data)
+
+
+class StringParser(IParser):
+    def parse(self, data: IO) -> Iterable[str]:
+        return map(str, data)
