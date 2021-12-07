50b04710d0f77b0f4ce70f5624bde4d0e453741a
Ian Laird <irlaird@gmail.com>
2021-12-06 20:53:44 -0700

    Added one-line integer parser.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aocfw",
-    version="0.1.0",
+    version="0.1.1",
     description="Advent Of Code Python Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
