9f0cb4b9276c2b5469ef1b7337aa459957948554
Ian Laird <irlaird@gmail.com>
2021-12-06 20:52:58 -0700

    Added one-line integer parser.

--- aocfw/__init__.py
+++ aocfw/__init__.py
@@ -1,5 +1,5 @@
 from .base import SolutionBase
 from .loaders import AutoLoader
-from .parsers import IntegerParser, StringParser
+from .parsers import IntegerParser, StringParser, OneLineIntegerParser
 from .test import TestCaseMixin
 from .typing import IParser, ILoader
