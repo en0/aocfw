c0dc5f8f76ef54d66922c79680e857da6b2226e0
Ian Laird <irlaird@gmail.com>
2021-12-09 00:33:05 -0700

    Added an auto submitter to try out.

--- aocfw/cli/run_solution.py
+++ aocfw/cli/run_solution.py
@@ -13,22 +13,47 @@ class RunSolutionEntryPoint(EntryPointBase):
             help="Download the given day's input and save it to a file."
         )
         ap.add_argument(
-            "path",
-            help="The path to the file containing the solution.",
-            type=str,
+            "--auto-submit",
+            default=False,
+            required=False,
+            action="store_true",
+            help="Submit the answer to AoC",
+        )
+        ap.add_argument(
+            "--day",
+            help="The day to run",
+            required=True,
+            type=int,
+        )
+        ap.add_argument(
+            "--part",
+            help="The part to run",
+            required=True,
+            type=int,
         )
         return ap
 
     def run(self) -> Optional[int]:
         from importlib import import_module
-        from os import path, chdir
+        from os import path, chdir, getcwd
         import sys
-        dirn = path.dirname(self.opts.path)
-        base = path.basename(self.opts.path)
+        dirn = f"{self.opts.day:02}"
+        mod = f"p{self.opts.part}"
+
+        pwd = getcwd()
         chdir(dirn)
+
         sys.path.insert(0, '')
-        module = import_module(base[:-3])
+        module = import_module(mod)
         solution = getattr(module, "Solution")
         ans = solution.check(source="input.txt")
         self.log.info("Answer: %s", ans)
-        #self.aoc_client.submit_answer(self.opts.day, self.config.get_year(), self.opts.part, ans)
+
+        chdir(pwd)
+        if self.opts.auto_submit:
+            result = self.aoc_client.submit_answer(
+                day=self.opts.day,
+                year=self.config.get_year(),
+                part=self.opts.part,
+                value=ans)
+            self.log.info(result)
