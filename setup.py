e74c1a1e4224498aaa1aa6ce28a9323df8b3474e
Ian Laird <irlaird@gmail.com>
2021-12-12 13:34:47 -0700

    Fixing setup to reference correct package name.

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="aocfw",
-    version="1.1.0",
+    version="1.1.1",
     description="Advent Of Code Python Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Ian Laird",
     author_email="irlaird@gmail.com",
     url="https://github.com/en0/aocfw",
-    packages=["aocfw", "aocfw.cli", "aocfw.client", "aocfw.lib"],
+    packages=["aocfw", "aocfw.cli", "aocfw.client", "aocfw.adt"],
     install_requires=["pyioc3", "requests", "beautifulsoup4"],
     entry_points={"console_scripts": ['aocfw=aocfw.cli.entry:main']},
     classifiers=[
